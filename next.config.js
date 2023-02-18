58e3e88c1da1fd5a1c8b2bee3348a6c6c38303ee
Megan Sharon <megan@sprouted.dev>
2023-02-18 17:50:27 -0500

    debug deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    runtime: "edge",
+    runtime: "experimental-edge",
     appDir: true,
     swcMinify: true,
   },
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+export const config = {
+  runtime: "experimental-edge"
+}
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  return new Response(JSON.stringify({ name: process.env.NEXT_RUNTIME }), {
+    headers: { "content-type": "application/json" },
+    status: 200
+  });
 }
