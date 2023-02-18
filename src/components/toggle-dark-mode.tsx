79c15209e894d07d16601a21d90180dffa414c2b
Megan Sharon <megan@sprouted.dev>
2023-02-18 13:07:34 -0500

    create color scheme enum

--- src/components/toggle-dark-mode.tsx
+++ src/components/toggle-dark-mode.tsx
@@ -3,35 +3,43 @@
 import { MouseEvent, useEffect, useState } from "react";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
+export enum ColorScheme {
+  Light = "light",
+  Dark = "dark",
+}
+
 export default function ToggleDarkMode() {
   const { matchMedia, localStorage } = window;
   const storedColorScheme = localStorage.getItem("theme");
   const systemPrefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
   const preferredColorScheme =
     storedColorScheme ||
-    (!storedColorScheme && systemPrefersDark ? "dark" : "light");
+    (!storedColorScheme && systemPrefersDark
+      ? ColorScheme.Dark
+      : ColorScheme.Light);
 
   const [colorMode, toggleColorMode] = useState(preferredColorScheme);
 
   const updateColorScheme = () => {
-    const newColorScheme = colorMode === "light" ? "dark" : "light";
+    const newColorScheme =
+      colorMode === ColorScheme.Light ? ColorScheme.Dark : ColorScheme.Light;
     localStorage.setItem("theme", newColorScheme);
     toggleColorMode(newColorScheme);
   };
   useEffect(() => {
-    if (colorMode === "dark") {
-      document.documentElement.classList.add("dark");
+    if (colorMode === ColorScheme.Dark) {
+      document.documentElement.classList.add(ColorScheme.Dark);
     } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove(ColorScheme.Dark);
     }
   }, [colorMode]);
 
   return (
     <div onClick={updateColorScheme} className="flex items-center">
-      {colorMode === "dark" ? (
-        <MoonIcon className="h-6 w-6 text-slate-50" />
+      {colorMode === ColorScheme.Light ? (
+        <MoonIcon className="h-6 w-6 text-slate-700" />
       ) : (
-        <SunIcon className="h-6 w-6 text-slate-700" />
+        <SunIcon className="h-6 w-6 text-slate-50" />
       )}
     </div>
   );
