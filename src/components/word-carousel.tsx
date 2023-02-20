5fdee979a7f79155ba411d72c62a0040f2c5134b
Megan Sharon <megan@sprouted.dev>
2023-02-20 14:56:24 -0500

    fix mobile

--- src/components/word-carousel.tsx
+++ src/components/word-carousel.tsx
@@ -8,7 +8,9 @@ export default function WordCarousel({ phrases }: WordCarouselProps) {
         <div className={`relative animate-[rotate_10s_ease-in-out_infinite]`}>
           {phrases.map((phrase, index) => (
             <div className="block h-20" key={index}>
-              <h1 className="text-6xl text-slate-50">{phrase}</h1>
+              <h1 className="text-2xl text-slate-50 md:text-4xl xl:text-6xl">
+                {phrase}
+              </h1>
             </div>
           ))}
         </div>
